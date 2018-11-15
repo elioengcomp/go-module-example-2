432cf5a2ae0c071c56cd145c79e9e850ed5e42c8
Elio Marcolino <eliom@jfrog.com>
2018-11-15 10:40:04 -0800

    Added bad import

--- example.go
+++ example.go
@@ -3,6 +3,7 @@ package example
 import (
 	"fmt"
 
+	log "github.com/Sirupsen/logrus"
 	example_3 "github.com/elioengcomp/go-module-example-3"
 )
 
@@ -10,5 +11,6 @@ const version = "v1.0.3"
 
 func Exec() string {
 
+	log.SetLevel(log.WarnLevel)
 	return fmt.Sprintf("This is go-module-example-2 %s consuming go-module-example-3: %s", version, example_3.Exec())
 }
